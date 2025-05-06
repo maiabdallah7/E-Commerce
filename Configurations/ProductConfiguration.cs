6ddc17a2aef2bcdb38d04cd4d741650f5b81e0f3
maiabdallah7 <maiabdalaa2019@gmail.com>
2025-05-07 00:17:39 +0300

    clear structure

---
+++ Configurations/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using E_commerce;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey("Id");
+        }
+    }
+}
